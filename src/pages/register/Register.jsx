05fb43164f8f804592b189f136a4d8e5f83afe5c
Osama-Najmi <osama.najmi18@gmail.com>
2023-03-09 15:46:14 +1100

    first commit

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -20,7 +20,10 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      await axios.post("http://localhost:8800/api/auth/register", inputs);
+      await axios.post(
+        "https://dahood.herokuapp.com/api/auth/register",
+        inputs
+      );
     } catch (err) {
       setErr(err.response.data);
     }
